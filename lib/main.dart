09545d39267f9703bd6707091d4d16aa6e45f471
Usamah <usamahaleale23@gmail.com>
2025-07-09 08:38:08 +0700

    Upload awal semua file by Usamah

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,22 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import '../../utils/app_routes.dart';
-import '../../utils/constants.dart';
+import 'utils/app_routes.dart';
+import 'utils/constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inisialisasi GetStorage
-  await GetStorage.init();
+  await GetStorage.init(); // init local storage
 
-  // Inisialisasi Supabase
-  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
 
   runApp(const MyApp());
 }
 
-// Helper global untuk akses mudah ke client Supabase
 final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
@@ -29,7 +29,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Supabase',
-      theme: ThemeData(primarySwatch: Colors.teal, useMaterial3: true),
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        useMaterial3: true,
+      ),
       initialRoute: AppRoutes.splash,
       getPages: AppRoutes.routes,
       debugShowCheckedModeBanner: false,
